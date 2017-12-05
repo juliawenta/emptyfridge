4a2193f05ddee0080ccc1605948e7d89feea46c6
juliawenta <wenta.julia@gmail.com>
2017-12-05 18:40:41 +0100

    Add second screen (first part)

--- main.c
+++ main.c
@@ -33,6 +33,8 @@ int main() {
 	}
 
 	print_welcome_screen(display);
+	print_meal_selection(display);
+
 
 	ALLEGRO_MOUSE *mouse;
 	al_install_mouse();
@@ -44,7 +46,7 @@ int main() {
 
 	al_flip_display(display);
 		
-	al_rest(4.0);
+	al_rest(40.0);
 
 	al_destroy_display(display);
 	return 0;
@@ -97,11 +99,11 @@ void print_textbox(char* input_text, ALLEGRO_DISPLAY *display) {
 	ALLEGRO_EVENT_QUEUE * queue = al_create_event_queue();
 	ALLEGRO_EVENT event;
 	
-	al_register_event_source(queue, al_get_keyboard_event_source());
-	//al_register_event_source(queue, al_get_mouse_event_source());
-	
-	
+	al_register_event_source(queue, al_get_keyboard_event_source()); //register keyboard
 
+	//al_register_event_source(queue, al_get_mouse_event_source());
+
+	
 	event.keyboard.unichar = NULL;
 
 	while (live) {
@@ -131,26 +133,36 @@ void print_textbox(char* input_text, ALLEGRO_DISPLAY *display) {
 			}
 		}
 		}
-		if (event.keyboard.unichar == 13) live = false;
-		
+		if (event.keyboard.unichar == 13) live = false; //13 is enter in ascii
+	
 		//i++;
 		if (i > 99)live = false;
 		event.keyboard.unichar = NULL;
 
-		
-
-
 	}
 
 }
-void print_meal_selection(ALLEGRO_DISPLAY *second_display)
+void print_meal_selection(ALLEGRO_DISPLAY *display)
 {
-	//ALLEGRO_DISPLAY *second_display = al_create_display(800, 600);
-	if (!second_display)
-	{
-		printf("Failed to create display.\n");
-		return 1;
-	}
+	al_clear_to_color(al_map_rgb(255, 255, 255));
+	al_draw_rectangle(50, 40, 750, 560, al_map_rgb(144, 144, 144), 2); //the big box
+	al_draw_line(50, 120, 750, 120, al_map_rgb(144, 144, 144), 2); //line which separate title from recipies
+	al_draw_line(50, 180, 750, 180, al_map_rgb(144, 144, 144), 1); //recipe 1
+	al_draw_line(50, 260, 750, 260, al_map_rgb(144, 144, 144), 1); //recipe 1
+	al_draw_line(50, 300, 750, 300, al_map_rgb(144, 144, 144), 1); //recipe 2
+	al_draw_line(50, 380, 750, 380, al_map_rgb(144, 144, 144), 1); //recipe 2
+	al_draw_line(50, 420, 750, 420, al_map_rgb(144, 144, 144), 1); //recipe 3
+	al_draw_line(50, 500, 750, 500, al_map_rgb(144, 144, 144), 1); //recipe 3
+
+	ALLEGRO_FONT *font = al_load_font("FantasqueSansMono-Regular.ttf", 22, NULL);
+	al_draw_text(font, al_map_rgb(253, 143, 0), 60, 48, ALLEGRO_ALIGN_LEFT, "There are three ideas of meals based on your products,");
+	al_draw_text(font, al_map_rgb(253, 143, 0), 60, 77, ALLEGRO_ALIGN_LEFT, "please select one of them:");
+
+	ALLEGRO_FONT *font1 = al_load_font("FantasqueSansMono-Regular.ttf", 20, NULL);
+	al_draw_text(font1, al_map_rgb(150, 150, 150), 60, 210, ALLEGRO_ALIGN_LEFT, "first");
+	al_draw_text(font1, al_map_rgb(100, 100, 100), 60, 330, ALLEGRO_ALIGN_LEFT, "second");
+	al_draw_text(font1, al_map_rgb(70, 70, 70), 60, 450, ALLEGRO_ALIGN_LEFT, "third");
+
 
 }
 void split_string(char* input_text, char** ingridients) {
